656645a19f04b3180c5ca943b4838ae94f5946c8
Charles <chuang93@gmail.com>
2016-10-15 22:05:19 -0500

    added Redux Store with Reducer Dependency and installed Socket.io

--- src/core.js
+++ src/core.js
@@ -9,6 +9,7 @@ function getWinners(vote) {
   else if (aVotes < bVotes)  return [b];
   else                       return [a, b];
 }
+export const INITIAL_STATE = Map();
 
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
@@ -31,9 +32,9 @@ export function next(state) {
 	}
 }
 
-export function vote(state, entry) {
-  return state.updateIn(
-    ['vote', 'tally', entry],
+export function vote(voteState, entry) {
+  return voteState.updateIn(
+    ['tally', entry],
     0,
     tally => tally + 1
   );
--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -5,6 +5,14 @@ import reducer from '../src/reducer';
 
 describe('reducer', () => {
 
+  it('has an initial state', () => {
+    const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
+    const nextState = reducer(undefined, action);
+    expect(nextState).to.equal(fromJS({
+      entries: ['Trainspotting']
+    }));
+  });
+
   it('handles SET_ENTRIES', () => {
     const initialState = Map();
     const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
@@ -48,5 +56,21 @@ describe('reducer', () => {
       entries: []
     }));
   });
+
+  it('can be used with reduce', () => {
+  const actions = [
+    {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']},
+    {type: 'NEXT'},
+    {type: 'VOTE', entry: 'Trainspotting'},
+    {type: 'VOTE', entry: '28 Days Later'},
+    {type: 'VOTE', entry: 'Trainspotting'},
+    {type: 'NEXT'}
+  ];
+  const finalState = actions.reduce(reducer, Map());
+
+  expect(finalState).to.equal(fromJS({
+    winner: 'Trainspotting'
+  }));
+});
 
 });
